7119ceba4436361ef4181756bcae44ed14c10989
Nguyễn Thành Luân <luannguyen@prep.vn>
2025-09-13 10:51:04 +0700

    feat: demo-dom

--- js/main.js
+++ js/main.js
@@ -1,2 +1,43 @@
-const p = prompt("Enter your name");
-alert("Hello " + p);
+/* function greet() {
+    console.log("Hello, world!");
+}
+
+const greet4 = function(){
+    console.log("Hello, world!");
+}
+greet();
+
+//ECMA Script 2017
+//ES6
+//context 
+const greet2 = () => {
+    console.log("Hello, world!");
+}
+
+//new function 
+const greet3 = new Function("console.log('Hello, world!')");
+ */
+
+
+/* const  greet = function(){
+    console.log("Hello, world!");
+}
+
+let greet2 = greet;
+greet2(); */
+
+
+//callback function 
+//higher order function 
+const test = function(callback){
+    console.log("call");
+    callback();
+}
+
+test(function(){
+    console.log("Hello, world!");
+});
+
+test(function(){
+    console.log("Hello, world! 2");
+})
